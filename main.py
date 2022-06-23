1092a375db478567a81c7fe7b6f875fa756f9d63
brian-hornbrook <brian_hornbrook@outlook.com>
2022-06-23 13:37:29 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+user_input = input("Enter a person's name: ")
+
+if (user_input == "Tomas Edison"):
+    print("Thomas's birthday is: 02/11/1847")
+
+elif (user_input == "Henry Ford"):
+    print("Henry's birthday is: 07/30/1863")
+
+elif (user_input == "Ada Lovelace"):
+    print("Ada's birthday is: 12/10/1815")
+
+else:
+    print("can't find a birthday!")
